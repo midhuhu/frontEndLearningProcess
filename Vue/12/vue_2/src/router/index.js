d14e8ae2359a9be1baa8b781f8403088737514f2
midhuhu <midhuhu@163.com>
2021-06-01 18:10:52 +0800

    update code

--- Vue/12/vue_2/src/router/index.js
+++ Vue/12/vue_2/src/router/index.js
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Three from '../views/Three.vue'
-import Four from '../views/Four.vue'
-import Five from '../views/Five.vue'
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'Home',
     component: Home
   },
@@ -17,22 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/three',
-    name: 'Three',
-    component: Three
-  },
-  {
-    path: '/four',
-    name: 'Four',
-    component: Four
-  },
-  {
-    path: '/five',
-    name: 'Five',
-    component: Five
-  },
+  }
 ]
 
 const router = createRouter({
