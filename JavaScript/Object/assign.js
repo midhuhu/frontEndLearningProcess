ed73efc60dbc427055c78feb19d2935e416e8224
zlq <midhuhu@163.com>
2022-07-07 15:07:35 +0800

    [mod] update

--- JavaScript/Object/assign.js
+++ JavaScript/Object/assign.js
@@ -10,24 +10,26 @@
 const target = { a: 1, b: 1 }
 const sources = { b: 4, c: 8 }
 const returnPoint = Object.assign(target, sources)
+// { a: 1, b: 4, c: 8 }
 
 target.a = 5
 returnPoint.b = 0
-sources.b = 10  // 不会改变return的值和target的值
+sources.b = 10  // 不会改变returnPoint的值和target的值
 
-// console.log(returnPoint);
-// console.log(target);
-// console.log(sources);
+// console.log(returnPoint)    // { a: 5, b: 0, c: 8 }
+// console.log(target)     // { a: 5, b: 0, c: 8 }
+// console.log(sources)    // { b: 10, c: 8 }
 
 /**
  *  深拷贝
  */
-const deepReturn = JSON.parse(JSON.stringify(target))
-target.a = 0
-// console.log('------------------');
-// console.log(returnPoint);
-// console.log(target);
-// console.log(deepReturn);  // deep clone
+const target1 = { a: 1, b: 1 }
+const deepReturn = JSON.parse(JSON.stringify(target1))
+// { a: 1, b: 1 }
+target1.a = 0
+// { a: 0, b: 1 }
+// console.log(target1)    // { a: 0, b: 1 }
+// console.log(deepReturn);  // deep clone { a: 1, b: 1 }
 
 /**
  *  合并对象
@@ -38,8 +40,7 @@ const o2 = { b: 2, c: 2 };
 const o3 = { c: 3 };
 
 const obj = Object.assign({}, o1, o2, o3);
-// console.log('------------------');
-// console.log(obj);
+// console.log(obj)    // { a: 1, b: 2, c: 3 }
 
 /**
  *  拷贝symbol 属性
@@ -48,37 +49,36 @@ const o4 = { a: 1 };
 const o5 = { [Symbol('a')]: 2 };
 const obj2 = Object.assign({}, o4, o5);
 
-// console.log('------------------');
-// console.log(obj2);
-// console.log(Object.getOwnPropertySymbols(obj2));
+// console.log(obj2)    // { a: 1, [Symbol('a')]: 2 }
+// console.log(Object.getOwnPropertySymbols(obj2))     // [Symbol(a)]
 
 /**
  *  原型链 和 不可枚举属性 不可被复制
  */
 const obj3 = Object.create(
-    { a: 1 },  //  a is prototype chain.
+    { a: 1 },  //  a is prototype chain（原型链）.
     {
-        bar: { value: 2 }, // bar is not enumerable.
+        bar: { value: 2 }, // bar is not enumerable(不可枚举).
         baz: { value: 3, enumerable: true }
     }
 );
 const copy = Object.assign({}, obj3);
-// console.log('------------------');
-// console.log(copy);
+
+// console.log(copy)   // { baz: 3 }
 
 /**
  *  基本类型
  *  Primitives will be wrapped, null and undefined will be ignored.
  *  Note, only string wrappers can have own enumerable properties.
  */
-const v1 = 'abc';
-const v2 = true;
-const v3 = 10;
-const v4 = Symbol('foo');
+const v1 = 'abcd'       // 只有v1可以被复制
+const v2 = true
+const v3 = 10
+const v4 = Symbol('foo')
 
-const copy2 = Object.assign({}, v2, v4);
-// console.log('------------------');
-// console.log(copy2);
+const copy2 = Object.assign({}, v1, v2, v3, v4)
+
+console.log(copy2)      // { '0': 'a', '1': 'b', '2': 'c', '3': 'd' }
 
 /**
  *  异常会打断后续操作
@@ -98,15 +98,15 @@ const copy3 = Object.assign({}, obj5);
 //TODO 实现过程
 function completeAssign(target, ...sources) {
     sources.forEach(source => {
-        console.log(1, source);
-        console.log(2, Object.keys(source));
+        // console.log(1, source);
+        // console.log(2, Object.keys(source));
         let descriptors = Object.keys(source).reduce((descriptors, key) => {
-            console.log(3, key, descriptors);
+            // console.log(3, key, descriptors);
             descriptors[key] = Object.getOwnPropertyDescriptor(source, key)
-            console.log(4, descriptors);
+            // console.log(4, descriptors);
             return descriptors
         }, {})
-        console.log(5, descriptors);
+        // console.log(5, descriptors);
         Object.getOwnPropertySymbols(source).forEach(sym => {
             let descriptor = Object.getOwnPropertyDescriptor(source, sym)
             if (descriptor.enumerable) {
@@ -121,5 +121,5 @@ function completeAssign(target, ...sources) {
 const m1 = { a: 1, b: 2 };
 const m2 = { b: 3, c: 4 };
 const obj6 = completeAssign({}, m1, m2);
-console.log('------------------');
-console.log(obj6);
+// console.log('------------------');
+// console.log(obj6);
--- JavaScript/Object/defineProperties.js
+++ JavaScript/Object/defineProperties.js
@@ -1,4 +1,18 @@
 /**
  *  chrome 5  / edge 12
  *  直接在一个对象上定义新的属性或修改现有属性，并返回该对象
+ *  Object.defineProperties(obj, props)
  */
+
+var obj = {};
+Object.defineProperties(obj, {
+    'property1': {
+        value: true,
+        writable: true
+    },
+    'property2': {
+        value: 'Hello',
+        writable: false
+    }
+    // etc. etc.
+});
